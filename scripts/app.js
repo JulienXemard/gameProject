12e628c463d384dc62dd8f33371540b4bafe9436
Julien Xemard <julien.xemard@outlook.com>
2020-07-19 11:39:18 +0200

    added data-id + css

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,44 @@
 function init() {
 
-  // DOM elements
+  //--------------------- DOM elements ---------------------//
   // All tracks
   const introTrack = document.querySelector('#intro-track')
-  const countDownSound = document.querySelector('#countDown-Sound')
+  const countDownSound = document.querySelector('#countDown-sound')
   const emperorLaugh = document.querySelector('#emperor-laugh')
-  const emperorYes = document.querySelector('#emperor-yes')
+  const emperorGood = document.querySelector('#emperor-good')
   //--------------------------------------------------------------
   const docBody = document.querySelector('body')
   const intro = document.querySelector('#intro')
   const orignalOption = document.querySelector('#original-btn')
   const sosOption = document.querySelector('#sos-btn')
   const gameTimer = document.querySelector('#timer-text')
+  const optionButtons = document.querySelectorAll('.level')
 
 
   let score = document.querySelector('#score')
   let time = document.querySelector('#time')
   let lives = document.querySelector('#lives')
   
-  // Game variables
+  //--------------------- Game Variables -------------------//
   let timerCount = 5
 
-  // Functions
-  function playSoundIntro(e) {
-    console.log('body clicked')
+  //----------------------- Functions ----------------------//
+  // inGame Sounds
+  function playSound(e) {
     introTrack.play()
-
   }
 
+  // gameStart timer
   function gameStartTimer(e) {
     intro.remove()
-    emperorLaugh.play()
-    gameTimer.innerHTML = timerCount
 
+    if (e.currentTarget === orignalOption) {
+      emperorLaugh.play()
+    } else if (e.currentTarget === sosOption) {
+      emperorGood.play()
+    }
+
+    gameTimer.innerHTML = timerCount
     const countDown = setInterval(() => {
       timerCount--
       gameTimer.innerHTML = timerCount
@@ -46,7 +52,7 @@ function init() {
   }
   
   // Event Listener
-  docBody.addEventListener('click', playSoundIntro)
+  docBody.addEventListener('click', playSound)
   orignalOption.addEventListener('click', gameStartTimer)
   sosOption.addEventListener('click', gameStartTimer)
 
